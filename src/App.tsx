db2614a15416b4ef6979e83317da172407f39221
kat-ki <glraskat@gmail.com>
2023-05-14 12:39:49 +0200

    input and btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
+
+type TodosType = {
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState<TodosType[]>([])
+    let [newTitle, setNewTitle] = useState('')
+
+    const fetchFoo = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setTodos(json))
+    }
+
+    useEffect(() => {
+        fetchFoo()
+    }, [])
+
+    const showButtonHandler = () => {
+        fetchFoo()
+    }
+
+    const hideButtonHandler = () => {
+        setTodos([])
+    }
+
+    const addNewItem = () => {
+        const newItem = { userId: 1, id: todos.length + 1, title: newTitle, completed: false }
+        setTodos([newItem, ...todos])
+        setNewTitle('')
+    }
+
+    return (
+        <div className="App">
+            <button onClick={showButtonHandler}>show</button>
+            <button onClick={hideButtonHandler}>hide</button>
+            <div>
+                <Input setNewTitle={setNewTitle} newTitle={newTitle} />
+                <Button callBack={addNewItem} />
+            </div>
+
+
+            <ul>
+                {todos.map((el) => {
+                        return (
+                            <li>
+                                <span> {el.id} </span>
+                                <span> {el.title} </span>
+                                <input type={'checkbox'} checked={el.completed} />
+                            </li>
+                        )
+                    }
+                )}
+            </ul>
+
+        </div>
+    );
 }
 
 export default App;
